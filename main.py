ed58b96e4f7daec9df5d17b79c156b84feb72fe7
userderdead <96183105+userderdead@users.noreply.github.com>
2021-12-16 19:11:46 +0530

    main.py fixed

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import asyncio
 from pytgcalls import idle
 from driver.Akshi import call_py, bot
 
-async def mulai_bot():
+async def start_bot():
     print("[Akshi]: STARTING BOT CLIENT")
     await bot.start()
     print("[Akshi]: STARTING PYTGCALLS CLIENT")
     await call_py.start()
     await idle()
-    await pidle()
     print("[Akshi]: STOPPING BOT & USERBOT")
     await bot.stop()
-
+    
 loop = asyncio.get_event_loop()
-loop.run_until_complete(mulai_bot())
+loop.run_until_complete(start_bot())
+
