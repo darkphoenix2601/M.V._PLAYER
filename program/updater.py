aed0edd6e824958a3aaeac538d942b7e13fa8be8
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2022-01-18 21:03:32 +0530

    Update updater.py

--- program/updater.py
+++ program/updater.py
@@ -60,7 +60,7 @@ async def update_repo(_, message: Message):
         system("git pull -f && pip3 install -r requirements.txt")
         execle(sys.executable, sys.executable, "main.py", environ)
         return
-    await msg.edit("bot is **up-to-date** with [main](https://github.com/szsupunma/video-stream)", disable_web_page_preview=True)
+    await msg.edit("bot is **up-to-date** with [main](https://github.com/darkphoenix2601/M.V._PLAYER)", disable_web_page_preview=True)
 
 
 @Client.on_message(command(["restart", f"restart@{BOT_USERNAME}"]) & ~filters.edited)
