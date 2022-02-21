67548be96e4476ca6ee306294e1c8b5f44ac8d16
hubbyofwifey <99789799+hubbyofwifey@users.noreply.github.com>
2022-02-21 14:29:48 +0530

    Update private.py

--- program/private.py
+++ program/private.py
@@ -1,13 +1,51 @@
-import asyncio
-from time import time
 from datetime import datetime
-from program.filters import command
+from sys import version_info
+from time import time
+
+from config import (
+    ALIVE_IMG,
+    ALIVE_NAME,
+    BOT_NAME,
+    BOT_USERNAME,
+    GROUP_SUPPORT,
+    OWNER_NAME,
+    UPDATES_CHANNEL,
+)
+from program import __version__
+from driver.Akshi import user
+from driver.filters import command, other_filters
 from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram import __version__ as pyrover
+from pytgcalls import (__version__ as pytover)
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+
+__major__ = 0
+__minor__ = 2
+__micro__ = 1
+
+__python_version__ = f"{version_info[0]}.{version_info[1]}.{version_info[2]}"
 
 
-@Client.on_message(command("void") & filters.private & ~filters.group & ~filters.edited)
-async def start_(client: Client, message: Message):
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ("week", 60 * 60 * 24 * 7),
+    ("day", 60 * 60 * 24),
+    ("hour", 60 * 60),
+    ("min", 60),
+    ("sec", 1),
+)
+
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return "inf"
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append("{} {}{}".format(amount, unit, "" if amount == 1 else "s"))
+    return ", ".join(parts):
     await message.reply_photo(
         photo=f"https://telegra.ph/file/38ca11b0fb33d6d9cf472.jpg",
         caption=f"""**━━━━━━━━━━━━━━━━━━
